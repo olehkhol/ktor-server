5b441b61af31bf2c663a2bf990375185248ca253
Oleh Kholiavchuk <oleh.khol@gmail.com>
2024-10-13 18:14:02 +0300

    Implement a Task Model

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,5 +1,7 @@
 package com.example.plugins
 
+import com.example.model.tasks
+import com.example.model.tasksAsTable
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
@@ -17,13 +19,7 @@ fun Application.configureRouting() {
         get("/tasks") {
             call.respondText(
                 contentType = ContentType.parse("text/html"),
-                text = """
-                    <h3>TODO:</h3>
-                    <ol>
-                        <li>A table of all the tasks</li>
-                        <li>A form to submit new taks</li>
-                    </ol>
-                """.trimIndent()
+                text = tasks.tasksAsTable(),
             )
         }
     }
