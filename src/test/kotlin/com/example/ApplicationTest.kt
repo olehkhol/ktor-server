ba25499ac73b08f06f9e8bf6bedfd925eb8e9ebd
Oleh Kholiavchuk <oleh.khol@gmail.com>
2024-10-13 17:37:35 +0300

    Write an integration test

---
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -0,0 +1,35 @@
+package com.example
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ApplicationTest {
+
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/")
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("Hello World!", response.bodyAsText())
+    }
+
+    @Test
+    fun testNewEndpoint() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/test1")
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("html", response.contentType()?.contentSubtype)
+        assertEquals("<h1>Hello From Ktor</h1>", response.bodyAsText())
+    }
+}
